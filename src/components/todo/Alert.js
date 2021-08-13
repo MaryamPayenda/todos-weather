491082ab00c32b79cd95ee38cc0e8f17a056a650
MaryamPayenda <maryampayanda12@gmail.com>
2021-08-13 16:20:17 +0200

    added

--- src/components/todo/Alert.js
+++ src/components/todo/Alert.js
@@ -1,18 +1,18 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 
-const Alert = ({ type, msg, removeAlert, list }) => {
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      removeAlert();
-    }, 3000);
-    return () => clearTimeout(timeout);
-  }, [list]);
+// const Alert = ({ type, msg, removeAlert, list }) => {
+//   useEffect(() => {
+//     const timeout = setTimeout(() => {
+//       removeAlert();
+//     }, 3000);
+//     return () => clearTimeout(timeout);
+//   }, [list]);
 
-  return (
-    <div>
-      <p className={`alert alert-${type}`}>{msg}</p>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <p className={`alert alert-${type}`}>{msg}</p>
+//     </div>
+//   );
+// };
 
-export default Alert;
+// export default Alert;
